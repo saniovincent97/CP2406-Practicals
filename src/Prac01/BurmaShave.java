27a5a77ea37f51fa8fd32314a060d3e8b547edf5
jc313927 <broderickwsthomsen@gmail.com>
2017-08-04 09:36:45 +1000

    Add new files; Update Comments.java

---
+++ src/Prac01/BurmaShave.java
@@ -0,0 +1,11 @@
+package Prac01;
+
+import javax.swing.JOptionPane;
+public class BurmaShave {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Goodbye! Shaving brush,");
+        JOptionPane.showMessageDialog(null, "Half a pound for half a dollar.");
+        JOptionPane.showMessageDialog(null, "Very fine for the skin. Druggists have it.");
+        JOptionPane.showMessageDialog(null, "Cheer up face the war is over. Burma Shave.");
+    }
+}
