5f4311de38caa505c84e0cc75110db816788a181
jc313927 <broderickwsthomsen@gmail.com>
2017-08-04 09:49:51 +1000

    Add FavouriteSong.java to Prac01

---
+++ src/Prac01/FavouriteSong.java
@@ -0,0 +1,9 @@
+package Prac01;
+
+public class FavouriteSong {
+    public static void main(String[] args){
+        System.out.println("I'll be the roundabout\nThe words will make you out 'n' out\nI spend the day your way\n" +
+                "Call it morning driving through the sound and\nIn and out the valley\n");
+        System.out.println("Yes - Roundabout(1972)");
+    }
+}
